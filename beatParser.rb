524ef559b35344056ae27b546c99db0158f5f695
stahnirockt <robert_owen@gmx.de>
2018-10-08 12:09:58 +0200

    upload new file for repo
    mainfile to play nested rings and arrays

---
+++ beatParser.rb
@@ -0,0 +1,106 @@
+def setSounds(str = 'path/to/foxdot_snd/')
+  #this is the path to /foxdot_snd - folder
+
+  @path = str
+
+  #this hash is the dictionary for the sounds -> each letter one soundfolder
+  @sounds = {:a => @path+'a/lower/',
+             :b => @path+'b/lower/',
+             :c => @path+'c/lower/',
+             :d => @path+'d/lower/',
+             :e => @path+'e/lower/',
+             :f => @path+'f/lower/',
+             :g => @path+'g/lower/',
+             :h => @path+'h/lower/',
+             :i => @path+'i/lower/',
+             :j => @path+'j/lower/',
+             :k => @path+'k/lower/',
+             :l => @path+'l/lower/',
+             :m => @path+'m/lower/',
+             :n => @path+'n/lower/',
+             :o => @path+'o/lower/',
+             :p => @path+'p/lower/',
+             :q => @path+'q/lower/',
+             :r => @path+'r/lower/',
+             :s => @path+'s/lower/',
+             :t => @path+'t/lower/',
+             :u => @path+'u/lower/',
+             :v => @path+'v/lower/',
+             :w => @path+'w/lower/',
+             :x => @path+'x/lower/',
+             :y => @path+'y/lower/',
+             :z => @path+'z/lower/',
+             :A => @path+'a/upper/',
+             :B => @path+'b/upper/',
+             :C => @path+'c/upper/',
+             :D => @path+'d/upper/',
+             :E => @path+'e/upper/',
+             :F => @path+'f/upper/',
+             :G => @path+'g/upper/',
+             :H => @path+'h/upper/',
+             :I => @path+'i/upper/',
+             :J => @path+'j/upper/',
+             :K => @path+'k/upper/',
+             :L => @path+'l/upper/',
+             :M => @path+'m/upper/',
+             :N => @path+'n/upper/',
+             :O => @path+'o/upper/',
+             :P => @path+'p/upper/',
+             :Q => @path+'q/upper/',
+             :R => @path+'r/upper/',
+             :S => @path+'s/upper/',
+             :T => @path+'t/upper/',
+             :U => @path+'u/upper/',
+             :V => @path+'v/upper/',
+             :W => @path+'w/upper/',
+             :X => @path+'x/upper/',
+             :Y => @path+'y/upper/',
+             :Z => @path+'z/upper/',
+             :hyphen => @path+'_/hyphen/',
+             :whitespace => 'silent'}
+end
+
+#check every stringelement and create rings or arrays using recursive functions
+def checkStringElement(scanner, pattern)
+  if scanner.scan(/(\w\:\d+)/) # e.g. "c:23"
+    soundBank = scanner[0].scan(/\w+/) # soundBank is an array [letter, number] as string
+    pattern << (sample_paths @sounds[soundBank[0].to_sym])[soundBank[1].to_i]
+  elsif scanner.scan(/\w/) # one letter e.g. "a"
+    pattern << @sounds[scanner[0].to_sym]
+  elsif scanner.scan(/\-\:\d+/) # e.g. "-:23"
+    soundBank = scanner[0].scan(/\d+/) # soundBank is just a number
+    pattern << (sample_paths @sounds[:hyphen])[soundBank[0].to_i]
+  elsif scanner.scan(/\-/)
+    pattern << @sounds[:hyphen]
+  elsif scanner.scan(/\ /)
+    pattern << @sounds[:whitespace]
+  elsif scanner.scan(/\[/)
+    arryResult = createArray(scanner) # call function to create an array
+    pattern << arryResult
+  elsif scanner.scan(/\(/) != nil
+    arryResult = createArray(scanner) # call function to create an array
+    pattern << arryResult.ring        # convert result to a ring
+  else
+    scanner.scan(/./)
+  end
+end
+
+# recursive funtion to create arrays for the beatpattern
+def createArray(scanner)
+  pattern = []
+  until scanner.scan(/\]/) or scanner.scan(/\)/)
+    checkStringElement(scanner, pattern)
+  end
+  return pattern
+end
+
+# function to parse the string with checkstring
+# return a ring containing elements, rings or/and arrays
+def parseBeatString(str)
+  pattern = []
+  scanner = StringScanner.new(str)
+  until scanner.pos == str.length
+    checkStringElement(scanner, pattern)
+  end
+  pattern.ring
+end
---
+++ beatPatternFns.rb
@@ -0,0 +1,99 @@
+require_relative 'beatParser.rb'
+
+# function to parse the string with checkstring
+# return a ring containing elements, rings or/and arrays
+def parseBeatString(str)
+  pattern = []
+  scanner = StringScanner.new(str)
+  until scanner.pos == str.length
+    checkStringElement(scanner, pattern)
+  end
+  pattern.ring
+end
+
+
+def samplePattern(pattern, *args)
+  args_h = resolve_synth_opts_hash_or_array(args)
+
+  # check if position, beat_duration and mode are set,
+  # if set --> set variable and delete value from hash
+  # otherwise set standart values for position, beat_duration and mode
+
+  if args_h[:position]
+    position = args_h.delete(:position)
+  else
+    position = tick
+  end
+
+  if args_h[:beat_duration]
+    sleep_time = args_h.delete(:beat_duration)
+  else
+    sleep_time = 1.0/pattern.length()
+  end
+
+  if args_h[:mode]
+    mode = args_h.delete(:mode)
+  else
+    mode = :sample
+  end
+
+  element = pattern[position] #select actual element
+  passing = position/pattern.length() #calculate passing
+
+  #check the element and decide
+  if element.kind_of?(Array)
+    playNestedArray(element, sleep_time, passing, mode, args_h)
+  elsif element.kind_of?(SonicPi::Core::RingVector)
+    alternateRingElement(element, sleep_time, passing, mode, args_h)
+  else
+    #element is just a sample or note
+    handleElement(element, mode, args_h)
+    sleep sleep_time
+  end
+end
+
+def playNestedArray(pattern, toplevel_sleep, toplevel_passing, mode, args_h)
+  sleep_time = toplevel_sleep*1.0/pattern.length() # set new time to sleep depending on toplevel_sleep and length of the array
+  pattern.each do |element|
+    if element.kind_of?(Array)
+      playNestedArray(element, sleep_time, toplevel_passing, mode, args_h)
+    elsif element.kind_of?(SonicPi::Core::RingVector)
+      alternateRingElement(element, sleep_time, toplevel_passing, mode, args_h)
+    else
+      handleElement(element, mode, args_h)
+      sleep sleep_time
+    end
+  end
+end
+
+
+def alternateRingElement(ring_pattern, toplevel_sleep, toplevel_passing, mode, args_h)
+  ring_passing = toplevel_passing/ring_pattern.length()
+  element = ring_pattern[toplevel_passing]
+  if element.kind_of?(SonicPi::Core::RingVector)
+    alternateRingElement(element, toplevel_sleep ,ring_passing, mode ,args_h)
+  elsif element.kind_of?(Array)
+    playNestedArray(element, toplevel_sleep, ring_passing, mode, args_h)
+  else
+    handleElement(element, mode, args_h)
+    sleep toplevel_sleep
+  end
+end
+
+def handleElement(element, mode, args_h)
+  case mode
+  when :sample
+    sample element, args_h
+  when :play
+    play element, args_h
+  end
+end
+
+def setupBeat(beat_string, sample_path: false)
+  if sample_path
+    setSounds(sample_path)
+  else
+    setSounds()
+  end
+  return parseBeatString(beat_string)
+end
