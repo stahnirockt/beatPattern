c7916438e08fdf2cc368c56ea8a62a4bf84601a7
stahnirockt <robert_owen@gmx.de>
2018-10-17 09:52:11 +0200

    new file to parse a melodie
    parse a string like '60 62 (65 r) 62' into a (nestes) ring with note values.

---
+++ melodieParser.rb
@@ -0,0 +1,45 @@
+def parseMelodieString(melodie_string)
+  melodiePattern = []
+  scanner = StringScanner.new(melodie_string)
+  until scanner.pos == melodie_string.length
+    checkMelodyStringElement(scanner, melodiePattern)
+  end
+  melodiePattern.ring
+end
+
+#check every stringelement and create rings or arrays using recursive functions
+def checkMelodyStringElement(scanner, pattern)
+  if scanner.scan(/\d+/) # one letter e.g. "a"
+    pattern << scanner[0].to_i
+  elsif scanner.scan(/r|\-/) # one letter e.g. "a"
+    pattern << :r
+  elsif scanner.scan(/\[/)
+    arryResult = createMelodieArray(scanner) # call function to create an array
+    pattern << arryResult
+  elsif scanner.scan(/\(/) != nil
+    arryResult = createMelodieArray(scanner) # call function to create an array
+    pattern << arryResult.ring        # convert result to a ring
+  elsif scanner.scan(/\{/) != nil
+    arryResult = createRandomMelodieArray(scanner) # call function to create an array
+    pattern << arryResult
+  else
+    scanner.scan(/./)
+  end
+end
+
+# recursive funtion to create arrays for the beatpattern
+def createMelodieArray(scanner)
+  pattern = []
+  until scanner.scan(/\]/) or scanner.scan(/\)/)
+    checkMelodyStringElement(scanner, pattern)
+  end
+  return pattern
+end
+
+def createRandomMelodieArray(scanner)
+  pattern = []
+  until scanner.scan(/\]/) or scanner.scan(/\)/) or scanner.scan(/\}/)
+    checkMelodyStringElement(scanner, pattern)
+  end
+  return RandomArray.new(pattern)
+end
